dc8f48576f1a754e3fedaeec1352a06c6c70491a
Shohei Miyazaki <s.miyazaki1989@gmail.com>
2023-08-15 16:38:55 +0900

    initial commit

---
+++ Form1.cs
@@ -0,0 +1,15 @@
+namespace testFormApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello world");
+        }
+    }
+}
